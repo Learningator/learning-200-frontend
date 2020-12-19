002b9ad745bd4265149a567f21665a47ceddde9f
Ulisessg <ukisessg@gmail.com>
2020-12-18 20:18:15 -0600

    Fix url of user

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Input.scss";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Input.scss';
 
 export class Input extends Component {
   state = {
     data: {
-      username: "",
+      username: '',
     },
   };
 
@@ -21,20 +21,20 @@ export class Input extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="Input-Main-Container">
-          <div className="Input__Container">
+        <div className='Input-Main-Container'>
+          <div className='Input__Container'>
             <h3>Ingresa tu username de Platzi</h3>
 
-            <span>platzi.com/@{this.state.data.username}</span>
+            <span>https://platzi.com/p/{this.state.data.username}</span>
             <input
-              placeholder="Ingresa tu username de Platzi"
-              type="text"
-              name="username"
-              id="username"
+              placeholder='Ingresa tu username de Platzi'
+              type='text'
+              name='username'
+              id='username'
               onChange={this.handleChange}
             />
 
-            <Link to="/ds">
+            <Link to='/ds'>
               <button>Comenzar</button>
             </Link>
           </div>
