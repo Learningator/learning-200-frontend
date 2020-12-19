ed763076f9b82f79133535239ebeffb3239469df
KevinJZea <kevinjzea@gmail.com>
2020-12-18 23:56:39 -0600

    KZ. LP, Input & Home pages. LearningPath page is up to date and working. So are Input & Home pages.

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "./Course.scss";
 
 export class Course extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="Course__Container">
-          <img src={this.props.image} alt="Course Logo" />
-          <h3>{this.props.title}</h3>
-          <span>
-            Instruido por: <strong>{this.props.teacher}</strong>
-          </span>
-          <p>{this.props.hours} horas de contenido</p>
+        <div className="Course__Main-Container">
+          <a href={this.props.url}>
+            <div className="Course__Container">
+              <img src={this.props.image} alt="Course Logo" />
+              <h3>{this.props.title}</h3>
+              <span>
+                Instruido por: <strong>{this.props.teacher}</strong>
+              </span>
+              <div className="Course__Container-Data">
+                <span>{this.props.level}</span>
+                <span>{this.props.hours} horas de contenido</span>
+              </div>
+            </div>
+          </a>
         </div>
       </React.Fragment>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,6 @@
-
 import React, { Component } from "react";
 import AreaCard from "../../components/AreaCard/AreaCard";
 import "./Home.scss";
-
 
 const DataScience =
   "https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Fds.png?alt=media&token=cde28f7e-ae5f-4227-aab1-d44b3954d11a";
@@ -15,21 +13,21 @@ export class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className='Main-Grid-container'>
-          <div className='Grid-container'>
+        <div className="Main-Grid-container">
+          <div className="Grid-container">
             <AreaCard
-              soon='Pr&oacute;ximamente'
-              area='Front-End'
-              className='Container-disabled'
-              to='/soon'
-              image='https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Ffrontend.png?alt=media&token=6799eba8-60fb-46c3-ba73-4d7a4d0e1911'
+              soon="Pr&oacute;ximamente"
+              area="Front-End"
+              className="Container-disabled"
+              to="/soon"
+              image={FrontEnd}
             />
             <AreaCard
-              soon='Pr&oacute;ximamente'
-              area='Back-End'
-              className='Container-disabled'
-              to='/soon'
-              image='https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Fbackend.png?alt=media&token=3bb5b3de-2375-42e5-8f3c-7b23bae9e5c2'
+              soon="Pr&oacute;ximamente"
+              area="Back-End"
+              className="Container-disabled"
+              to="/soon"
+              image={BackEnd}
             />
             <AreaCard
               className="Container-enabled"
