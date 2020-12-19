fe91cb785ec633ba76be9a62c407b039871a7c8c
KevinJZea <kevinjzea@gmail.com>
2020-12-18 19:35:58 -0600

    KZ. Fixing conflicts in Home.js. Just fixing conflicts with the images.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import AreaCard from "../../components/AreaCard/AreaCard";
-import DataScience from "../../images/ds.png";
-import FrontEnd from "../../images/frontend.png";
-import BackEnd from "../../images/backend.png";
-
 import "./Home.scss";
+
+const DataScience =
+  "https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Fds.png?alt=media&token=cde28f7e-ae5f-4227-aab1-d44b3954d11a";
+const FrontEnd =
+  "https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Ffrontend.png?alt=media&token=6799eba8-60fb-46c3-ba73-4d7a4d0e1911";
+const BackEnd =
+  "https://firebasestorage.googleapis.com/v0/b/learningator.appspot.com/o/images%2Fbackend.png?alt=media&token=3bb5b3de-2375-42e5-8f3c-7b23bae9e5c2";
 
 export class Home extends Component {
   render() {
